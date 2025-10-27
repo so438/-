5f8155aa3d448bdeb8330809eb1d1b7c4e96ebca
so438 <sohee0123@naver.com>
2025-10-27 10:13:18 +0900

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -6,7 +6,7 @@ app.get("/edi", function (req: any, res: any) {
     req.query;
 
   var api_url =
-    "https://apis.data.go.kr/B551182/mcatInfoService1.2/getPaymentNonPaymentList1.2?";
+    "http://apis.data.go.kr/B551182/mcatInfoService1.2/getPaymentNonPaymentList1.2?";
   var request = require("request");
   var options = {
     url: api_url,
@@ -26,6 +26,6 @@ app.get("/edi", function (req: any, res: any) {
 
 app.listen(3000, function () {
   console.log(
-    "https://127.0.0.1:3000/edi?serviceKey=2a48f93a5348af17db0d725602e187d8bbb0569c6092441acaf20573b0985fb0&numOfRows=10&pageNo=1&mcatCd=G8207118 app listening on port 3000!"
+    "http://127.0.0.1:3000/edi?serviceKey=2a48f93a5348af17db0d725602e187d8bbb0569c6092441acaf20573b0985fb0&numOfRows=10&pageNo=1&mcatCd=G8207118 app listening on port 3000!"
   );
 });
